5f8a967b074cbe388cb2c345cc760db189dcac35
Mike Lee <mike.seung.lee@gmail.com>
2014-11-19 01:59:50 -0800

    Profile may be trickier than I think.  Also the height issue for footer is still buggy, especially for profile which is long

--- mobile/static/js/controllers/profileController.js
+++ mobile/static/js/controllers/profileController.js
@@ -1,3 +1,18 @@
-function profileController($scope) {
+function profileController($scope, $http) {
+    var num = 2;
+    $scope.editMode = true;
+    $http.get('api/users/'+ num + '/').
+        success(function(res) {
+            console.log(res);
+            $scope.user = res;
+            $scope.user.email = 'testing@test.com';
+        }).
+        error(function(err) {
+            console.log(err);
+    });
 
+
+    $scope.toggleEdit = function() {
+        $scope.editMode = !$scope.editMode;
+    }
 }
